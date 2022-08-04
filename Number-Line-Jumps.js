91317606814dfa93e1a77f2daff1c54572ca316d
Airam Toscano <airamlobato@gmail.com>
2022-08-04 11:01:28 -0400

    Feat: fix Number-line

--- Number-Line-Jumps.js
+++ Number-Line-Jumps.js
@@ -4,6 +4,7 @@ function kangaroo(x1, v1, x2, v2) {
 
 // x1 + k*v1 == x2 + k*v2 a formula do problema
 // (x1 - x2) == k*(v2 - v1) 
+// (x2 - x1) === k* (v1 - v2)
 // e isso k deve ser um inteiro sem sinal, ou seja, 
 // devemos verificar que (x1 - x2)é um múltiplo de (v2 - v1). 
 // Além disso, seus sinais devem ser os mesmos, 
