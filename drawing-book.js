56a0d96efcb7be2e0108510713bd5be3ee1a571c
Airam Toscano <airamlobato@gmail.com>
2022-09-23 10:46:39 -0400

    Feat: drawing-book

---
+++ drawing-book.js
@@ -0,0 +1,22 @@
+function pageCount(n, p) {
+    let cont1dif =  (p - 1) / 2
+    let cont2dif = (n - p) / 2
+    console.log(cont1dif, cont2dif)
+    if(cont1dif % 2 ==0 ) {
+        let real = Math.ceil(cont2dif)
+        let real2 = Math.ceil(cont1dif)
+        if(real2 < real) return real2
+        if(real < real2) return real
+        return real
+    }
+    if(cont1dif % 2 !=0 ) {
+        let real = Math.floor(cont2dif)
+        let real2 = Math.ceil(cont1dif)
+        if(real2 < real) return real2
+        if(real < real2) return real
+        return real
+    }
+}
+
+
+console.log(pageCount(6,5))
