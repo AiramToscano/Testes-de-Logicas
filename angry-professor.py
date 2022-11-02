fa459a85b67deedb7c87ba4485aaabced96dc131
Airam Toscano <airamlobato@gmail.com>
2022-11-02 10:54:16 -0400

    Feat: angry-professor.py

---
+++ angry-professor.py
@@ -0,0 +1,11 @@
+def angryProfessor(k, a):
+    cont = 0
+    for i in a:
+        if(i < 2):
+            cont += 1
+    if(cont >= k):
+        return 'NO'
+    return 'YES'
+
+vet = [-1,-3,5,2]
+print(angryProfessor(3,vet))
